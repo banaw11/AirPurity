7b30c09e8939af138b9fdda3b0e841f3b7731079
banaw11 <banaw11@gmail.com>
2022-06-18 01:54:31 +0200

    Deploy

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -89,11 +89,6 @@ namespace API.Extensions
                     options.UseNpgsql(connStr);
                 }
 
-                // Whether the connection string came from the local development configuration file
-                // or from the environment variable from Heroku, use it to set up your DbContext.
-
-
-
             });
             services.AddScoped<IValidator<CityQuery>, CityQueryValidator>();
             services.AddScoped<IValidator<SensorsDataQuery>, SensorsDataQueryValidator>();
